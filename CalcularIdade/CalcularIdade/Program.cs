faee216ad5b486356d117dd12070937501605359
Marcio Alves de Oliveira <64289932+madeoliveira@users.noreply.github.com>
2020-06-23 13:13:40 -0300

    Commit inicial

---
+++ CalcularIdade/CalcularIdade/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcularIdade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programa que calcula Idade");
+            Pessoa pessoa = new Pessoa();
+            Console.Write("Nome da pessoa: ");
+            pessoa.Nome = Console.ReadLine();
+            Console.Write("Ano de nascimento: ");
+            pessoa.AnoNascimento =Convert.ToInt32( Console.ReadLine());
+            pessoa.ExibirDados();
+            Console.ReadKey();
+        }
+    }
+}
